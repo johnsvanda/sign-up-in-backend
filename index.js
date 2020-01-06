3cfd2805ecc4b434241fc9784ccbc3f4997eb4c9
Jan Švanda <john.svanda@gmail.com>
2020-01-06 10:24:46 +0100

    protected trieda

--- index.js
+++ index.js
@@ -19,10 +19,10 @@ mongoose.connect(
 );
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(express.static(path.join(__dirname, "build")));
 
   app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+    res.sendFile(path.join(__dirname, "build", "index.html"));
   });
 }
 
