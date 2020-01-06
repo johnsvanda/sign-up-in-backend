d5146231d47ec570dce99d1d4aa231a85f0d7d0c
Jan Švanda <john.svanda@gmail.com>
2020-01-06 09:04:27 +0100

    changed routes

---
+++ build/precache-manifest.7f11dd5381909436b27f52561d0e362d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bacc15e182216c33aecc3e29fb9c47b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ea0a901434c1575545d",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "31f99901a769440f0aa0",
+    "url": "/static/css/main.d2556c4f.chunk.css"
+  },
+  {
+    "revision": "9ea0a901434c1575545d",
+    "url": "/static/js/2.089cc585.chunk.js"
+  },
+  {
+    "revision": "4aa0fe1eaee3dcf45338a38641d40f08",
+    "url": "/static/js/2.089cc585.chunk.js.LICENSE"
+  },
+  {
+    "revision": "31f99901a769440f0aa0",
+    "url": "/static/js/main.f4e96fc8.chunk.js"
+  },
+  {
+    "revision": "05e2d2637e83c42dc136",
+    "url": "/static/js/runtime-main.4ee28378.js"
+  },
+  {
+    "revision": "ba853d577a497425f02d096c667bcc4a",
+    "url": "/static/media/background.ba853d57.jpg"
+  }
+]);
--- index.js
+++ index.js
@@ -18,11 +18,13 @@ mongoose.connect(
   }
 );
 
-app.use(express.static(path.join(__dirname, "build")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "build")));
 
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+  });
+}
 
 app.get("/json", (req, res) => {
   res.json({
